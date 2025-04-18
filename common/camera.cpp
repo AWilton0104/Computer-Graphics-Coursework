cd9ba58432a6098b9dec72f731b22a1f99b3edef
AWilton0104 <146730082+AWilton0104@users.noreply.github.com>
2025-04-18 15:26:04 +0100

    Added work done in labs to coursework
    Added, maths, camera and shader implementations from my lab work. Implemented constraints on player to stay on floor and ability to jump.

--- common/camera.cpp
+++ common/camera.cpp
@@ -1,1 +1,73 @@
+#include <common/camera.hpp>
 
+Camera::Camera(const glm::vec3 Eye, const glm::vec3 Target)
+{
+    eye = Eye;
+    target = Target;
+}
+
+void Camera::calculateMatrices(float deltaTime)
+{
+    calculateCameraVectors();
+    if (!jumping) {
+        eye.y = 0;
+    }
+    else {
+        eye.y = jumpHeight * sin(3.14159f * jumpTimer);
+        jumpTimer -= deltaTime;
+        if (jumpTimer < 0) {
+            jumping = false;
+            jumpTimer = jumpTime;
+        }
+    }
+    view = glm::lookAt(eye, eye + front, worldUp);
+    projection = glm::perspective(fov, aspect, near, far);
+}
+
+void Camera::calculateCameraVectors()
+{
+    front = glm::vec3(cos(yaw) * cos(pitch), sin(pitch), sin(yaw) * cos(pitch));
+    right = glm::normalize(glm::cross(front, worldUp));
+    up = glm::cross(right, front);
+}
+
+void Camera::jump() {
+    jumping = true;
+}
+
+void Camera::quaternionCamera(float deltaTime)
+{
+    //Jumping and Keep Player at Floor Level
+    if (!jumping) {
+        eye.y = 0;
+    }
+    else {
+        eye.y = jumpHeight * sin(3.14159f * jumpTimer);
+        jumpTimer -= deltaTime;
+        if (jumpTimer < 0) {
+            jumping = false;
+            jumpTimer = jumpTime;
+        }
+    }
+
+    // Calculate camera orientation quaternion from the Euler angles
+    Quaternion newOrientation(-pitch, yaw);
+
+    // Apply SLERP
+    orientation = Maths::SLERP(orientation, newOrientation, 0.2f);
+
+    // Calculate the view matrix
+    view = orientation.matrix() * Maths::translate(-eye);
+
+    if (thirdPerson) {
+        view = Maths::translate(offset) * view;
+    }
+
+    // Calculate the projection matrix
+    projection = glm::perspective(fov, aspect, near, far);
+
+    // Calculate camera vectors from view matrix
+    right = glm::vec3(view[0][0], view[1][0], view[2][0]);
+    up = glm::vec3(view[0][1], view[1][1], view[2][1]);
+    front = -glm::vec3(view[0][2], view[1][2], view[2][2]);
+}
--- common/camera.hpp
+++ common/camera.hpp
@@ -1,1 +1,52 @@
+#pragma once
 
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <common/maths.hpp>
+
+class Camera
+{
+public:
+    // Projection parameters
+    float fov = Maths::radians(45.0f);
+    float aspect = 1024.0f / 768.0f;
+    float near = 0.2f;
+    float far = 100.0f;
+    float jumpTime = 1.0f;
+    float jumpTimer = jumpTime;
+    float jumpHeight = 2.5f;
+    bool jumping = false;
+    bool thirdPerson = true;
+    glm::vec3 offset = glm::vec3(0.0f, 0.0f, -1.0f);
+    Quaternion orientation = Quaternion(pitch, yaw);
+
+
+    // Camera vectors
+    glm::vec3 eye;
+    glm::vec3 target;
+    glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 right = glm::vec3(1.0f, 0.0f, 0.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
+
+    //Camera Angles
+    float yaw = 0.0f;
+    float pitch = 0.0f;
+    float roll = 0.0f;
+
+    // Transformation matrices
+    glm::mat4 view;
+    glm::mat4 projection;
+
+    // Constructor
+    Camera(const glm::vec3 eye, const glm::vec3 target);
+
+    // Methods
+    void calculateMatrices(float deltaTime);
+
+    void calculateCameraVectors();
+
+    void jump();
+
+    void quaternionCamera(float deltaTime);
+};
